5e76f9772200a794a24150fbac692cc30444686c
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-04-27 02:32:15 -0700

    initial commit - created new images, css, and js folders + files

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//Variables
+
+//API Key
+
+//Current weather
+//OpenWeather API url
+//Start fetch request 
+
+//Current city name
+//Current date
+//Temperature
+//Wind
+//Humidity
+//UV index
+
+//Five-day forecast
+//Date
+//Icon
+//Temperature
+//Wind
+//Humidity
+
+//Search button
+//localStorage - list of cities
