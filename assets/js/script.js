0625a672dbc6533d7a73a7339b3240756de1f407
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-04-30 07:39:44 -0700

    fixed 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,157 +1,143 @@
-//Variables
-//API Key
+// Variables
+// API Key
 var apiKey = "f6672cc6e9cb17c1050849aae4579382";
-
-//Search history
+// Search History
 var searchHistoryList = [];
-
-//Date - use moment.js
+// Date using Moment.js
 var today = moment().format('L');
 
-// Enter a city name and display the city's current weather condition 
-// Search result includes the current city, date, weather icon, temperature, humidity, wind speed, and UV index
-function currentWeather(cityname) {
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=imperial&appid=${apiKey}`;
-
+// Function for current condition
+function currentWeather(city) {
+    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     $.ajax({
-        url: queryURL,
+        url: currentWeatherURL,
         method: "GET"
-    }).then(function(cityWeatherResponse) {
-        console.log(cityWeatherResponse);
-
+    }).then(function (currentResponse) {
+        console.log(currentResponse);
         $("#weatherContent").css("display", "block");
         $("#cityDetail").empty();
+        var iconCode = currentResponse.weather[0].icon;
+        var weatherIconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-        var weatherIcon = cityWeatherResponse.weather[0].icon;
-        var weatherIconURL = `https://openweathermap.org/img/w/${weatherIcon}.png`;
-
+        // Display searched city's current weather condition
+        // Information includes current weather icon, temperature, humidity, wind speed, and uv index
         var currentCity = $(`
             <h2 id="currentCity">
-                ${cityWeatherResponse.name} ${today} <img src="${weatherIconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+            ${currentResponse.name} ${today} <img src="${weatherIconURL}" alt="${currentResponse.weather[0].description}" />
             </h2>
-            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
-            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
-            <p>Humidity: ${cityWeatherResponse.main.humidity} \%</p>
-            
+            <p>Temperature: ${currentResponse.main.temp} °F</p>
+            <p>Humidity: ${currentResponse.main.humidity}\%</p>
+            <p>Wind Speed: ${currentResponse.wind.speed} MPH</p>
         `);
-
         $("#cityDetail").append(currentCity);
 
-        //Display the UV Index
-        var lat = cityWeatherResponse.coord.lat;
-        var lon = cityWeatherResponse.coord.lon;
-        var uviQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`
-
+        // Display current UV index
+        var lat = currentResponse.coord.lat;
+        var lon = currentResponse.coord.lon;
+        var uviURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
         $.ajax({
-            url: uviQueryURL,
+            url: uviURL,
             method: "GET"
         }).then(function (uviResponse) {
             console.log(uviResponse);
-
             var uvIndex = uviResponse.value;
             var uvIndexP = $(`
                 <p>UV Index:
-                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+                <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
                 </p>
             `);
-
             $("#cityDetail").append(uvIndexP);
 
             futureWeather(lat, lon);
 
-            // Display UV Index color to indicate if the condition is favorable (green), moderate (yellow), or severe (orange, red, violet)
+            // Display UV index color to indicate condition
+            // Current UV condition can be favorable (0-2, green), moderate (3-5, yellow), or severe (6-7 orange; 8-10, red; 11+, violet)
             if (uvIndex >= 0 && uvIndex <= 2) {
-                $("#uvIndexColor").css("background-color", "#008000").css("color", "white");
+                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
             } else if (uvIndex >= 3 && uvIndex <= 5) {
-                $("#uvIndexColor").css("background-color", "#FFFF00");
+                $("#uvIndexColor").css("background-color", "#FFF300");
             } else if (uvIndex >= 6 && uvIndex <= 7) {
-                $("#uvIndexColor").css("background-color", "#FFA500");
+                $("#uvIndexColor").css("background-color", "#F18B00");
             } else if (uvIndex >= 8 && uvIndex <= 10) {
-                $("#uvIndexColor").css("background-color", "#FF0000").css("color", "white");
+                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
             } else {
-                $("#uvIndexColor").css("background-color", "#9400D3").css("color", "white");
+                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white");
             };
         });
     });
 }
 
-// Display the searched city's five-day weather forecast
-// Search result includes the date, weather icon, temperature, humidity, and wind speed
+// Function for 5-Day Weather Forecast
 function futureWeather(lat, lon) {
 
-    var futureURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
-    
+    var futureWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
     $.ajax({
-        url: futureURL,
+        url: futureWeatherURL,
         method: "GET"
-}).then(function(futureResponse) {
-    console.log(futureResponse);
-    $("#fiveDay").empty();
-
-    for (let i = 1; i < 6; i++) {
-        var cityInfo = {
-            date: futureResponse.daily[i].dt,
-            icon: futureResponse.daily[i].weather[0].icon,
-            temp: futureResponse.daily[i].temp.day,
-            wind: futureResponse.daily[i].wind.speed,
-            humidity: futureResponse.daily[i].humidity
-        };
-
-        var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-        var weatherIconURL = `<img src="https://openweathemap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
-
-        var futureCard = $(`
-        <div class="pl-3">
-            <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
+    }).then(function (futureResponse) {
+        console.log(futureResponse);
+        $("#fiveDay").empty();
+        for (let i = 1; i < 6; i++) {
+            var cityInfo = {
+                date: futureResponse.daily[i].dt,
+                icon: futureResponse.daily[i].weather[0].icon,
+                temp: futureResponse.daily[i].temp.day,
+                humidity: futureResponse.daily[i].humidity
+            };
+            var currentDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+            var weatherIconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
+            
+            // Display weather forecast for the next five days
+            // Future weather information includes weather icon, temperature, wind speed, and humidity
+            var futureWeatherCard = $(`
+                <div class="pl-3">
+                <div class="card pl-3 pt-3 mb-3 bg text-light" style="width: 12rem;>
                 <div class="card-body">
-                    <h5>${currDate}</h5>
-                    <p>${weatherIconURL}</p>
-                    <p>Temp: ${cityInfo.temp} °F</p>
-                    <p>Wind: ${cityInfo.wind.speed} MPH</>
-                    <p>Humidity: ${cityInfo.humidity}\%</p>
-                </div>
+                <h5>${currentDate}</h5>
+                <p>${weatherIconURL}</p>
+                <p>Temp: ${cityInfo.temp} °F</p>
+                <p>Humidity: ${cityInfo.humidity}\%</p>
             </div>
         </div>
-        `);
-
-        $("#fiveDay").append(futureCard);
-    }
-});
-
+    <div>
+`);
+            $("#fiveDay").append(futureWeatherCard);
+        }
+    });
 }
 
-//Add event listener
-$("#searchBtn").on("click", function(event) {
+// Event listener
+$("#searchBtn").on("click", function (event) {
     event.preventDefault();
-
-    var cityname = $("#enterCity").val().trim();
-    currentWeather(cityname);
-    if (!searchHistoryList.includes(cityname)) {
-        searchHistoryList.push(cityname);
+    var city = $("#enterCity").val().trim();
+    currentWeather(city);
+    if (!searchHistoryList.includes(city)) {
+        searchHistoryList.push(city);
         var searchedCity = $(`
-            <li class="list-group-item">${cityname}</li>
+            <li class="list-group-item">${city}</li>
         `);
 
+        // Display search history/searched cities  
         $("#searchHistory").append(searchedCity);
     };
 
+    // Searched cities are stored in localStorage
     localStorage.setItem("city", JSON.stringify(searchHistoryList));
     console.log(searchHistoryList);
 });
 
-// When a city in the search history is clicked, display the city's current weather and 5-day weather forecast 
-$(document).on("click", ".list-group-item", function() {
+// Display a city's current and future weather conditions when clicked from the search history list 
+$(document).on("click", ".list-group-item", function () {
     var listCity = $(this).text();
     currentWeather(listCity);
 });
 
-// When the application is opened, display the last searched city forecast
-$(document).ready(function() {
-    var searchHistoryArray = JSON.parse(localStorage.getItem("cityname"));
-
+// Display the the last searched city's weather conditions when the application is opened
+$(document).ready(function () {
+    var searchHistoryArray = JSON.parse(localStorage.getItem("city"));
     if (searchHistoryArray !== null) {
         var lastSearchedIndex = searchHistoryArray.length - 1;
-        var lastSearchedCity =searchHistoryArray[lastSearchedIndex];
+        var lastSearchedCity = searchHistoryArray[lastSearchedIndex];
         currentWeather(lastSearchedCity);
         console.log(`Last searched city: ${lastSearchedCity}`);
     }
